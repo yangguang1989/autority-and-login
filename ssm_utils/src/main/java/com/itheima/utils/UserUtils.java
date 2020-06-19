97695011d5e3b58ea7522367a70181042e425846
杨广 <7455879+yang_guang_1992@user.noreply.gitee.com>
2020-06-19 17:48:10 +0800

    增加一个工具从安全框架获取user对象的类

---
+++ ssm_utils/src/main/java/com/itheima/utils/UserUtils.java
@@ -0,0 +1,23 @@
+package com.itheima.utils;
+
+import com.itheima.domain.SysUser;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+public class UserUtils {
+    public static User getLoginUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            if (authentication instanceof AnonymousAuthenticationToken) {
+                return null;
+            }
+            if (authentication instanceof UsernamePasswordAuthenticationToken) {
+                return (User) authentication.getPrincipal();
+            }
+        }
+        return null;
+    }
+}
